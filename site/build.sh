fddb5f51999bef314f66c54c4b30b1c2564e0e42
Rogério Carvalho Schneider <stockrt@gmail.com>
2009-08-03 01:12:21 -0300

    Big changes on the site, cleanup made

--- site/build.sh
+++ site/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-rm -rf ../p ../page[0-9]* ../tags
+FROM_SITE=$(find . -maxdepth 1 -mindepth 1 | cut -d/ -f2-)
+for E in $FROM_SITE
+do
+    rm -rf ../$E
+done
+rm -rf ../p ../page[0-9]*
+
 jekyll ../
 mv ../atom.html ../atom.xml
 rm -f ../*.sh
