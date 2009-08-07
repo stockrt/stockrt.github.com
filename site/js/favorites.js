ef1a004b619b841456b70cb720e28b62af50c86b
Rogério Carvalho Schneider <stockrt@gmail.com>
2009-08-07 01:52:56 -0300

    Favorites

--- site/js/favorites.js
+++ site/js/favorites.js
@@ -4,6 +4,3 @@ function addToFavorites(title, url) {
     else if (window.sidebar)
         window.sidebar.addPanel(title, url, "");
 }
-//function newWindow(url) {
-//    popupWindow = window.open(url, "popUpWindow", "height=600,width=600,left=50,top=50,resizable=yes,scrollbars=1,toolbar=no,menubar=no,location=no,directories=no,status=no");
-//}
