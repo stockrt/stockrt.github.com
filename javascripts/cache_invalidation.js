34e0cd32d0b195a853ad1e74d3d28246288c69c2
Rogério Carvalho Schneider <stockrt@gmail.com>
2009-08-21 22:12:28 -0300

    Testing cache_invalidation

--- javascripts/cache_invalidation.js
+++ javascripts/cache_invalidation.js
@@ -1,19 +1,46 @@
+/*
+ * Author: Rogério Carvalho Schneider <stockrt@gmail.com>
+ * URL:    http://stockrt.github.com
+ *
+ * Invalidates browser's cache for a given page based on the last refresh.
+ *
+ * Usage:
+ * - Include in the HEAD of your page and set the TTL
+ *
+ * <html>
+ *
+ * <head>
+ *  <script src="http://your_site/javascripts/querystring.js" type="text/javascript"></script>
+ *  <script src="http://your_site/javascripts/cache_invalidation.js" type="text/javascript"></script>
+ * </head>
+ *
+ * <body>
+ * </body>
+ *
+ * </html>
+ *
+ * - And set the TTL you want into the cache_invalidation.js file:
+ *    // TTL: set your cache threshold here
+ *    var ttl = 300;  // seconds
+ */
 
-  function rl(now) {
+// TTL: set your cache threshold here
+var ttl = 300;  // seconds
+
+var ttl = ttl * 1000;
+var qs = new Querystring();
+var ts = Number(qs.get("cache"));
+var now = Number(new Date().getTime());
+
+function rl(now) {
     var loc = window.location.href.replace(/\?.*/, "");
     window.location.href = loc+"?cache="+now;
-  }
-  var qs = new Querystring();
-  var ts = Number(qs.get("cache"));
-  var now = Number(new Date().getTime());
-  
-    var ttl = 300 * 1000;
-  
-  if (ts) {
+}
+
+if (ts) {
     var d = ts + ttl - now;
     if (d < 0)
-      rl(now);
-  }
-  else
+        rl(now);
+}
+else
     rl(now);
-
