718e3e9a6f18894ed7fa8a270600bffc7a88b9f7
Rogério Carvalho Schneider <stockrt@gmail.com>
2009-08-30 21:07:11 -0300

    Publishing new posts for today

--- site/publishtoday.sh
+++ site/publishtoday.sh
@@ -4,9 +4,12 @@ BD=$(dirname $0)
 D=$(date +"%Y-%m-%d")
 
 cd $BD
-sed -i -e '/^published: false$/d' _posts/${D}*
-./build.sh ok
-cd ..
-git add .
-git commit -a -m 'Publishing new posts for today'
-git push origin master
+sed -i -e '/^published: false$/d' _posts/${D}* && export PUB=1 || export PUB=0
+if [[ "$PUB" == 1 ]]
+then
+    ./build.sh ok
+    cd ..
+    git add .
+    git commit -a -m 'Publishing new posts for today'
+    git push origin master
+fi
