ea6fc108b9bea7113b1ea30cce17ab7c15433d9a
Rogério Carvalho Schneider <stockrt@gmail.com>
2010-01-06 01:26:38 -0200

    add favorites change - removed sidebar option

--- site/javascripts/favorites.js
+++ site/javascripts/favorites.js
@@ -1,5 +1,4 @@
 // Add page to the browser's Favorites
 function addToFavorites(title, url) {
-    if (document.all)
-        window.external.AddFavorite(url, title);
+    window.external.AddFavorite(url, title);
 }
